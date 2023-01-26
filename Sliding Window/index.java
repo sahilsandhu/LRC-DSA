c53e95fc68dc04f4c94c01199151ce5a499b8c6a
sahil sandhu <46192764+sahilsandhu@users.noreply.github.com>
2023-01-27 00:56:02 +0530

    init

--- Sliding Window/index.java
+++ Sliding Window/index.java
@@ -48,48 +48,45 @@ class Solution {
 
 public int longestkSubstr(String s, int k) {
         int n = s.length();
-        int i = -1, j = -1;
-        int ans = -1;
         HashMap<Character, Integer> hm = new HashMap<>();
+        int i=-1, j=-1, ans=-1;
         while(true){
             boolean f1 = false, f2 = false;
-            while(i < n-1){
-                f1 = true;
+            while(i<n-1){
                 i++;
                 char ch = s.charAt(i);
-                hm.put(ch, hm.getOrDefault(ch, 0)+1);
-                if(hm.size() < k)
+                hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+                if(hm.size()<k)
                     continue;
-                else if(hm.size() == k){
-                    int len = i-j;
-                    if(len > ans)
-                       ans = len;
-                }
-                else{
+                else if(hm.size()==k)
+                    ans = Math.max(ans, i-j);
+                else
                     break;
-                }
-            }
-            while(j < i){
                 f1 = true;
+            }
+            
+            while(j<i){
                 j++;
                 char ch = s.charAt(j);
-                if(hm.get(ch) == 1)
+                if(hm.get(ch)==1)
                     hm.remove(ch);
                 else
                     hm.put(ch, hm.get(ch)-1);
-                
-                if(hm.size() > k){
+                    
+                if(hm.size() > k)
                     continue;
-                }
                 else if(hm.size() == k){
                     int len = i-j;
                     if(len > ans)
                        ans = len;
                     break;
                 }
+                //j++;
+                f2 = true;
             }
-            if(f1 == false && f2 == false)
-            break;
+            if(f1 == false && f2 == false){
+                break;
+            }
         }
         return ans;
     }
